9d28bb9fb4077aac73ab6e1bb083edb50c96a4f8
Christian Gessner <christian@gessner.cc>
2022-02-16 15:03:51 +0100

    Added Create STL workflow

---
+++ AUTOMATE/Create STL from Inventor Model/README.md
@@ -0,0 +1,30 @@
+[![powerJobs](https://img.shields.io/badge/powerJobs_Processor-22.0.19-orange.svg)](https://www.coolorange.com/powerjobs)
+
+# Create STL from Inventor Model
+
+## Description
+This workflow uses coolOrange **powerJobs** to create a STL file from an Inventor model (IPT or IAM).
+
+## Prerequisites
+[powerJobs Processor](https://www.coolorange.com/powerjobs) version 22.0.19 or later must be installed on the Vault Job Processor machine(s)  
+
+## Job Settings
+The settings of jobs can be adjusted using the [powerJobs  settings dialog](https://doc.coolorange.com/projects/coolorange-powerjobsprocessordocs/en/stable/job_configuration/#powerjobs-settings-dialog).
+
+## Job Trigger
+In order to configure the job to be executed on a file lifecycle state transition, Vault's Custom Job Types functionality can be used. [More information](https://doc.coolorange.com/projects/coolorange-powerjobsprocessordocs/en/stable/getting_started/#how-to-embed-the-job-in-a-status-change)
+
+## Additional Ressources
+
+### STL File Format
+[https://en.wikipedia.org/wiki/STL_(file_format)](https://en.wikipedia.org/wiki/STL_(file_format))
+
+### STL File Save As Options reference
+[https://knowledge.autodesk.com/support/inventor-products/learn-explore/caas/CloudHelp/cloudhelp/2015/ENU/Inventor-Help/files/GUID-59FB702C-F37A-4DEA-ADCF-51AD8A219C9B-htm.html](https://knowledge.autodesk.com/support/inventor-products/learn-explore/caas/CloudHelp/cloudhelp/2015/ENU/Inventor-Help/files/GUID-59FB702C-F37A-4DEA-ADCF-51AD8A219C9B-htm.html)
+
+## Disclaimer
+
+THE SAMPLE CODE ON THIS REPOSITORY IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+THE USAGE OF THIS SAMPLE IS AT YOUR OWN RISK AND **THERE IS NO SUPPORT** RELATED TO IT.
+
+The usage of these samples is at your own risk. There is no free support related to the samples. However, if you have any questions, you can visit [https://doc.coolorange.com/](https://doc.coolorange.com/) for product documentations or you can start a conversation in our support forum at [http://support.coolorange.com/support/discussions](http://support.coolorange.com/support/discussions)
---
+++ AUTOMATE/Create STL from Inventor Model/powerJobs/Jobs/COOLORANGE.CreateSTL.ps1
@@ -0,0 +1,152 @@
+#==============================================================================#
+# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER    #
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.   #
+#                                                                              #
+# Copyright (C) 2022 COOLORANGE S.r.l.                                         #
+#==============================================================================#
+
+if (-not $IAmRunningInJobProcessor) {
+    # for manual script execution
+    Import-Module powerJobs
+    Open-VaultConnection
+    $file = Get-VaultFile -Properties @{Name="Wheel.ipt"}
+    $workingDirectory = "C:\TEMP\powerJobs Processor\" + (New-Object System.Guid).ToString()
+}
+
+#region Settings
+# To include the Revision of the main file in the STL name set $true, otherwise $false
+$stlFileNameWithRevision = $true
+
+# The character used to separate file name and Revision label in the STL name such as hyphen (-) or underscore (_)
+$stlFileNameRevisionSeparator = "_"
+
+# To include the file extension of the main file in the STL name set $true, otherwise $false
+$stlFileNameWithExtension = $false
+
+# To add the STL to Vault set $true, to keep it out set $false
+$addSTLToVault = $true
+
+# To attach the STL to the main file set $true, otherwise $false
+$attachSTLToVaultFile = $true
+
+# Specify a Vault folder in which the STL should be stored (e.g. $/Designs/STL), or leave the setting empty to store the STL next to the main file
+$stlVaultFolder = ""
+
+# Specify a network share into which the STL should be copied (e.g. \\SERVERNAME\Share\Public\STLs\)
+$stlNetworkFolder = ""
+
+# STL specific settings - details can be found in Inventor API help
+# Format: 0=binary, 1=ASCII
+$stlOutputFileType = 0  
+
+# Resolution: 4=Brep, 3=Custom, 2=High, 1=Medium, 0=Low
+$stlResulution = 1
+
+#  Allow to move Internal Mesh Nodes; set $true, otherwise $false
+$stlAllowMoveMeshNode = $true
+
+# Units: INCH = 2, FOOT = 3, CENTIMETER = 4, MILLIMETER = 5, METER = 6, MICRON = 7
+$stlExportUnits = 4 
+#endregion
+
+#STL settings - details can be found in Inventor API help
+$stlSurfaceDeviation = 60  # Range 0 to 100, the value has precision of 0.0001. None-zero value is used if Resolution is CUSTOM (3), otherwise value is ignored.
+$stlNormalDeviation = 14  # Range 0 to 41. Non-zero value is used if Resolution is CUSTOM (3), otherwise value is ignored. Any input value out of this range will be changed to 14 by force.
+$stlMaxEdgeLength = 100  # Range 0 to 100. Non-zero value is used if Resolution is CUSTOM (3), otherwise value is ignored. 
+$stlAspectRatio = 40  # Range 0 to 21.5. Non-zero value is used if Resolution is CUSTOM (3), otherwise value is ignored.
+$stlExportFileStructure = 0 # ONE FILE = 0, ONE FILE PER PART INSTANCE = 1
+$stlExportColor = $true # Write color information to STL binary file.
+
+$stlFileName = [System.IO.Path]::GetFileNameWithoutExtension($file._Name)
+if ($stlFileNameWithRevision) {
+    $stlFileName += $stlFileNameRevisionSeparator + $file._Revision
+}
+if ($stlFileNameWithExtension) {
+    $stlFileName += "." + $file._Extension
+}
+$stlFileName += ".stl"
+
+if ([string]::IsNullOrWhiteSpace($stlVaultFolder)) {
+    $stlVaultFolder = $file._EntityPath
+}
+
+Write-Host "Starting job 'Create STL as attachment' for file '$($file._Name)' ..."
+
+if ( @("iam", "ipt") -notcontains $file._Extension ) {
+    Write-Host "Files with extension: '$($file._Extension)' are not supported"
+    return
+}
+if (-not $addSTLToVault -and -not $stlNetworkFolder) {
+    throw("No output for the STL is defined in ps1 file!")
+}
+if ($stlNetworkFolder -and -not (Test-Path $stlNetworkFolder)) {
+    throw("The network folder '$stlNetworkFolder' does not exist! Correct stlNetworkFolder in ps1 file!")
+}
+
+$file = (Save-VaultFile -File $file._FullPath -DownloadDirectory $workingDirectory)[0]
+$localSTLfileLocation = "$workingDirectory\$stlFileName"
+
+$openResult = Open-Document -LocalFile $file.LocalPath #-Application InventorServer
+if ($openResult) {
+    try {
+        $InvApp = $openResult.Application.Instance
+        $STLAddin = $InvApp.ApplicationAddIns | Where-Object { $_.ClassIdString -eq "{533E9A98-FC3B-11D4-8E7E-0010B541CD80}" }
+        $Context = $InvApp.TransientObjects.CreateTranslationContext()
+        $Options = $InvApp.TransientObjects.CreateNameValueMap()
+
+        if($openResult.Application.Name -eq "Inventor") {
+            Write-Host "Using Inventor..."
+            $SourceObject = $InvApp.ActiveDocument
+        } elseif ($openResult.Application.Name -eq "InventorServer") {
+            Write-Host "Using Inventor Server..."
+            $SourceObject = $InvApp.Documents[1]
+        } else {
+            throw "$($openResult.Application.Name) not supported"
+        }
+
+        if ($STLAddin.HasSaveCopyAsOptions($SourceObject, $Context, $Options)) {
+            $Options.Value("Resulution") = $STLResulution
+            $Options.Value("ExportUnits") = $stlExportUnits
+            $Options.Value("AllowMoveMeshNode") = $stlAllowMoveMeshNode
+            $Options.Value("SurfaceDeviation") = $stlSurfaceDeviation
+            $Options.Value("NormalDeviation") = $stlNormalDeviation
+            $Options.Value("MaxEdgeLength") = $stlMaxEdgeLength
+            $Options.Value("AspectRatio") = $stlAspectRatio
+            $Options.Value("ExportFileStructure") = $stlExportFileStructure
+            $Options.Value("OutputFileType") = $stlOutputFileType 
+            $Options.Value("ExportColor") = $stlExportColor
+        }
+
+        $oData = $InvApp.TransientObjects.CreateDataMedium()
+        $Context.Type = 13059       #kFileBrowseIOMechanism
+        $oData.MediumType = 56577   #kFileNameMedium
+        $oData.FileName = $localSTLfileLocation
+        $STLAddin.SaveCopyAs($SourceObject, $Context, $Options, $oData)
+        $exportResult = $true
+
+        $STLfile = Add-VaultFile -From $localSTLfileLocation -To "$stlVaultFolder/$stlFileName" -FileClassification DesignVisualization #None  
+        $file = Update-VaultFile -File $file._FullPath -AddAttachments @($STLfile._FullPath)
+    }
+    catch {
+        $exportResult = $false
+        $exportResult | Add-Member -NotePropertyName Error -NotePropertyValue $_.Exception
+    }
+    
+    $closeResult = Close-Document
+}
+
+if (-not $openResult) {
+    throw("Failed to open document $($file.LocalPath)! Reason: $($openResult.Error.Message)")
+}
+if (-not $exportResult) {
+    throw("Failed to export document $($file.LocalPath) to $localSTLfileLocation! Reason: $($exportResult.Error.Message)")
+}
+if (-not $closeResult) {
+    throw("Failed to close document $($file.LocalPath)! Reason: $($closeResult.Error.Message))")
+}
+if ($ErrorCopySTLToNetworkFolder) {
+    throw("Failed to copy STL file to network folder '$stlNetworkFolder'! Reason: $($ErrorCopySTLToNetworkFolder)")
+}
+
+Write-Host "Completed job 'Create STL as attachment'"
