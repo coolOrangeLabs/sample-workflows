2fcbdbccb8263e3f8ffb71068a3bbb4d5ed853be
ThomasRossmeisl <thomas.rossmeisl@coolorange.com>
2022-10-28 12:55:09 +0200

    * Refactored GetParts function to adhere more closely to common PowerShell and functional coding style   * Now returns a new list instead of modifying a list in higher scope   * Removed partList parameter as it isn't needed anymore   * Parameters are now written in pascal case   * Added $UsePositionNumber as parameter instead of accessing a global variable   * Removed -Recursive parameter from Get-VaultFileBom as it is called recursively already   * Using -f fromating operator instead of + to avoid issues with numeric values   * Removed Add-Member for "Multiplicator" property, as this is just a function parameter and not needed on the result objects   * Removed -ParentPosition parameter and used the position/RowOrder on the $Parent object instead   * Using -InputObject instead of pipe to call Add-Member   * Function is now called with named parameters to make it easier to understand what value is passed to what parameter * Unused values are now assigned to $null * Standard PowerShell is now used where appropriate instead of .NET functions * Using consistent formatting across the script now

--- Automation/Export BOM from Inventor Assembly/powerJobs/Jobs/COOLORANGE.Export.BOM.ps1
+++ Automation/Export BOM from Inventor Assembly/powerJobs/Jobs/COOLORANGE.Export.BOM.ps1
@@ -11,10 +11,10 @@
 
 #region Debug
 if (-not $IAmRunningInJobProcessor) {
-    Import-Module powerJobs
-    # https://doc.coolorange.com/projects/coolorange-powervaultdocs/en/stable/code_reference/commandlets/open-vaultconnection.html
-    Open-VaultConnection
-    $file = Get-VaultFile -Properties @{Name="Suspension.iam"}
+	Import-Module powerJobs
+	# https://doc.coolorange.com/projects/coolorange-powervaultdocs/en/stable/code_reference/commandlets/open-vaultconnection.html
+	Open-VaultConnection
+	$file = Get-VaultFile -Properties @{Name="Suspension.iam"}
 }
 #endregion
 
@@ -23,52 +23,66 @@ Write-Host "Starting job '$($job.Name)' for file '$($file._Name)'..."
 #region Settings
 # If checked the positions are filled with the 'Position Number' property of the Inventor BOM rows, otherwise the property 'Row Order' will be used
 $usePositionNumber = $false
-# Name of the CSV file that contains the BOM information 
+# Name of the CSV file that contains the BOM information
 $csvFileName = "$($file._Name)_StructuredBOM.csv"
 # Specify a network share into which the CSV should be copied (e.g. \\SERVERNAME\Share\Public\BOMs\)
 $csvNetworkFolder = "C:\Temp\BOMs"
 #endregion
 
 if( @("iam") -notcontains $file._Extension ) {
-    Write-Host "Files with extension: '$($file._Extension)' are not supported"
-    return
+	Write-Host "Files with extension: '$($file._Extension)' are not supported"
+	return
 }
 
-function GetParts($partList, $parent, $parentPosition="", $multiplicator = 1) {
-    $children = Get-VaultFileBom -File $parent._FullPath -Recursive
-	foreach ($child in $children) {
-        if ($usePositionNumber) {
-            $position = $parentPosition + "." + $child.Bom_PositionNumber
-        } else {
-            $position = $parentPosition + "." + $child.Bom_RowOrder
-        }
-        $position = $position.TrimStart('.')
-        $quantity = $child.Bom_Quantity
+function GetParts {
+	param(
+		$Parent,
+		$Multiplicator = 1,
+		[bool]$UsePositionNumber
+	)
 
-        $child | Add-Member -NotePropertyName Position -NotePropertyValue $position
-        $child | Add-Member -NotePropertyName Multiplicator -NotePropertyValue $multiplicator
-        $child | Add-Member -NotePropertyName Quantity -NotePropertyValue ($quantity)
-        $child | Add-Member -NotePropertyName TotalQuantity -NotePropertyValue ($multiplicator * $quantity)
-        $partList.Add($child)
+	$parts = @()
+
+	$children = Get-VaultFileBom -File $Parent._FullPath
+	foreach ($child in $children) {
+		if ($UsePositionNumber) {
+			$position = '{0}.{1}' -f $Parent.Bom_PositionNumber, $child.Bom_PositionNumber
+		}
+		else {
+			$position = '{0}.{1}' -f $Parent.Bom_RowOrder, $child.Bom_RowOrder
+		}
+		$position = $position.TrimStart('.')
+
+		Add-Member -InputObject $child -NotePropertyName Position -NotePropertyValue $position
+		Add-Member -InputObject $child -NotePropertyName Quantity -NotePropertyValue $child.Bom_Quantity
+		Add-Member -InputObject $child -NotePropertyName TotalQuantity -NotePropertyValue ($Multiplicator * $child.Bom_Quantity)
+
+		$parts += $child
 
 		if ($child._Extension -eq "iam") {
-			GetParts $partList $child $position ($multiplicator * $quantity)
+			$childParts = GetParts -Parent $child -Multiplicator ($Multiplicator * $child.Bom_Quantity) -UsePositionNumber $UsePositionNumber
+			if($childParts) {
+				$parts += $childParts
+			}
 		}
 	}
+	return ,$parts
 }
 
-$files = New-Object System.Collections.Generic.List[PSCustomObject]
-GetParts $files $file
+$partsList = GetParts -Parent $file -UsePositionNumber $usePositionNumber
 
-if ($files.Count -le 0) {
-    throw "Cannot find a Structured BOM in Vault for file '$($file._Name)'. Please open the file in Inventor and make sure the Structued BOM is enabled."
+if ($partsList.Count -le 0) {
+	throw "Cannot find a Structured BOM in Vault for file '$($file._Name)'. Please open the file in Inventor and make sure the Structued BOM is enabled."
 }
 
 if (-not (Test-Path -Path $csvNetworkFolder)) {
-    New-Item -ItemType "directory" -Path $csvNetworkFolder
+	$null = New-Item -ItemType Directory -Path $csvNetworkFolder
 }
 
-$csvFullFileName = [System.IO.Path]::Combine($csvNetworkFolder, $csvFileName)
-$files | Select-Object -Property Bom_Number, Position, Quantity, TotalQuantity, Bom_ItemQuantity, Bom_UnitQuantity, Bom_Structure | Export-Csv -Path $csvFullFileName -NoTypeInformation
+$csvFullFileName = Join-Path -Path $csvNetworkFolder -ChildPath $csvFileName
+
+$partsList `
+	| Select-Object -Property Bom_Number, Position, Quantity, TotalQuantity, Bom_ItemQuantity, Bom_UnitQuantity, Bom_Structure `
+	| Export-Csv -LiteralPath $csvFullFileName -NoTypeInformation
 
 Write-Host "Completed job '$($job.Name)'"
